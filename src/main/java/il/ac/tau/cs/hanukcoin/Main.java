6fe0e4f4f53f8660bc94b62b7e02b11dcd18fed4
DanielFH <danfh9@gmail.com>
2024-11-18 01:14:01 +0200

    created blocks and validated chain

---
+++ src/main/java/il/ac/tau/cs/hanukcoin/Main.java
@@ -0,0 +1,24 @@
+package il.ac.tau.cs.hanukcoin;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Block> blocks = new ArrayList();
+        blocks.add(HanukCoinUtils.createBlock0forTestStage());
+        for (int i = 0; i < 3; i++) {
+            Block previous = blocks.get(i);
+            blocks.add(HanukCoinUtils.mineCoinAttempt(
+                    // alternate between 2 wallets
+                    (i + 1) % 2,
+                    previous,
+                    10000000
+            ));
+        }
+        System.err.println(blocks);
+        System.err.println(HanukCoinUtils.checkBlockChainValidation(blocks));
+
+    }
+}
